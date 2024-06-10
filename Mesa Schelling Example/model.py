968e3da731e5b2d81c94d15716d04afac3830d65
ThomasTenDamme <ttdamme@gmail.com>
2024-06-10 10:24:17 +0200

    Analysis works and Homophily changed to work with fractions

--- Mesa Schelling Example/model.py
+++ Mesa Schelling Example/model.py
@@ -26,12 +26,34 @@ class SchellingAgent(mesa.Agent):
             if neighbor.type == self.type:
                 similar += 1
 
+        total_neighbours = (2 * self.model.radius + 1) ** 2 - 1
+        proportion_similar = similar / total_neighbours
+
         # If unhappy, move:
-        if similar < self.model.homophily:
+        if proportion_similar < self.model.homophily:
             self.model.grid.move_to_empty(self)
+            
+            # TODO: change to move to good cell
+            # for agent, loc in self.model.grid.coord_iter():
+                
+            
         else:
             self.model.happy += 1
 
+def get_segregation(model):
+    """
+    Find the % of agents that only have neighbors of their same type.
+    """
+    segregated_agents = 0
+    for agent in model.schedule.agents:
+        segregated = True
+        for neighbor in model.grid.iter_neighbors(agent.pos, True):
+            if neighbor.type != agent.type:
+                segregated = False
+                break
+        if segregated:
+            segregated_agents += 1
+    return segregated_agents / model.schedule.get_agent_count()
 
 class Schelling(mesa.Model):
     """
@@ -42,7 +64,7 @@ class Schelling(mesa.Model):
         self,
         height=20,
         width=20,
-        homophily=3,
+        homophily=0.3,
         radius=1,
         density=0.8,
         minority_pc=0.2,
@@ -73,7 +95,10 @@ class Schelling(mesa.Model):
 
         self.happy = 0
         self.datacollector = mesa.DataCollector(
-            model_reporters={"happy": "happy"},  # Model-level count of happy agents
+            model_reporters={"happy": "happy", "Segregated_Agents" : get_segregation},  # Model-level count of happy agents
+        
+            agent_reporters={"x": lambda a: a.pos[0], "y": lambda a: a.pos[1], "type": "type"}
+        
         )
 
         # Set up agents
@@ -88,7 +113,7 @@ class Schelling(mesa.Model):
                 self.schedule.add(agent)
 
         self.datacollector.collect(self)
-
+    
     def step(self):
         """
         Run one step of the model.
@@ -100,3 +125,4 @@ class Schelling(mesa.Model):
 
         if self.happy == self.schedule.get_agent_count():
             self.running = False
+            
